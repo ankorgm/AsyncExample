5e0501cafb4f3eca1057b100bc1523558412d716
ankorgm <ankorgm@gmail.com>
2022-12-31 02:30:26 +0300

    Finished with sparrow counter example

--- app/src/main/java/ru/synergy/asyncexample/ThreadExample.java
+++ app/src/main/java/ru/synergy/asyncexample/ThreadExample.java
@@ -1,14 +1,89 @@
 package ru.synergy.asyncexample;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ThreadExample extends AppCompatActivity {
+
+    int mCounter;
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            TextView mInfoTextViev = (TextView) findViewById(R.id.textViewInfo);
+            mInfoTextViev.setText("Сегодня ворон было  " + mCounter++ + "  штук");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread_example);
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                // do time consuming operations here
+//            }
+//        }).start();
+
+
     }
+//    private Runnable doInBackgroundProcessing = new Runnable() {
+//        @Override
+//        public void run() {
+//            BackgroundThreadProcessing();
+//        }
+//    };
+//
+//    private void BackgroundThreadProcessing() {
+//        // Трудоемкие операции
+//
+//    }
+//
+//    private void  mainProcessing(){
+//        Thread thread = new Thread(null, doInBackgroundProcessing, "Background");
+//
+//    }
+
+    public void onClick(View v) {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                long endTime = System.currentTimeMillis() + 20 * 1000;
+                while (System.currentTimeMillis() < endTime) {
+                    synchronized (this) {
+                        try {
+                            wait(endTime - System.currentTimeMillis());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    Log.i("SPARROWS", ("Сегодня ворон было  " + mCounter + "  штук"));
+
+                    handler.sendEmptyMessage(0);
+                }
+
+
+                // Так делать нельзя
+//                TextView mInfoTextViev = (TextView) findViewById(R.id.textViewInfo);
+//                mInfoTextViev.setText("Сегодня ворон было  " + mCounter++ + "  штук");
+            }
+        };
+
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+
+    }
+
 }
